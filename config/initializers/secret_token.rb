6f903bda0f78db477c152b3fe18be3271ec8f116
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2013-12-23 20:13:57 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mondai::Application.config.secret_key_base = '7e4de21450908afd07d0fa3e1e3280c5e4f4cfe87d1a3df29fe2aedc0d118b5fc7bfa8978ef0a829da18213392a221486f434600c5061cff36e84ab4a3c95ac5'
