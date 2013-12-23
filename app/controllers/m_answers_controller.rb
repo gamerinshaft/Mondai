0f9154a5475a7937aa10241790cdcad59bf1eee4
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2013-12-23 21:33:59 +0900

    write code -> m_answers_controller.rb

--- app/controllers/m_answers_controller.rb
+++ app/controllers/m_answers_controller.rb
@@ -1,6 +1,6 @@
 class MAnswersController < ApplicationController
   before_action :set_m_answer, only: [:show, :edit, :update, :destroy]
-
+  before_action :set_m_question, except: [:destroy]
   # GET /m_answers
   # GET /m_answers.json
   def index
@@ -19,16 +19,16 @@ class MAnswersController < ApplicationController
 
   # GET /m_answers/1/edit
   def edit
+    @m_answer = @m_question.m_answers.find(params[:id])
   end
 
   # POST /m_answers
   # POST /m_answers.json
   def create
-    @m_answer = MAnswer.new(m_answer_params)
-
+    @m_answer = @m_question.m_answers.build(m_answer_params)
     respond_to do |format|
       if @m_answer.save
-        format.html { redirect_to @m_answer, notice: 'M answer was successfully created.' }
+        format.html { redirect_to m_question_m_answer_path(@m_question,@m_answer), notice: 'M answer was successfully created.' }
         format.json { render action: 'show', status: :created, location: @m_answer }
       else
         format.html { render action: 'new' }
@@ -40,9 +40,10 @@ class MAnswersController < ApplicationController
   # PATCH/PUT /m_answers/1
   # PATCH/PUT /m_answers/1.json
   def update
+    @m_answer = @m_question.m_answers.build(m_answer_params)
     respond_to do |format|
       if @m_answer.update(m_answer_params)
-        format.html { redirect_to @m_answer, notice: 'M answer was successfully updated.' }
+        format.html { redirect_to m_question_m_answer_path(@m_question,@m_answer), notice: 'M answer was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
@@ -54,9 +55,10 @@ class MAnswersController < ApplicationController
   # DELETE /m_answers/1
   # DELETE /m_answers/1.json
   def destroy
+
     @m_answer.destroy
     respond_to do |format|
-      format.html { redirect_to m_answers_url }
+      format.html { redirect_to m_question_m_answers_url }
       format.json { head :no_content }
     end
   end
@@ -70,5 +72,9 @@ class MAnswersController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def m_answer_params
       params.require(:m_answer).permit(:check, :answer)
+    end
+
+    def set_m_question
+      @m_question = MQuestion.find(params[:m_question_id])
     end
 end
