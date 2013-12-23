e685b49fca8d2ff5856e4ddc36cc6474cfbad01e
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2013-12-23 21:09:49 +0900

    migration databse

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20131223120916) do
+
+  create_table "b_answers", force: true do |t|
+    t.string   "answer"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "b_questions", force: true do |t|
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "m_answers", force: true do |t|
+    t.boolean  "check"
+    t.string   "answer"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "m_questions", force: true do |t|
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
